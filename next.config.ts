74988024aa194d461daf1fe55f777fd6e7202880
abdallah <zaoe909@gmail.com>
2026-01-31 08:38:19 +0200

    Fix routing 404 by disabling trailingSlash for better compatibility with GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/portfello',
-  trailingSlash: true,
+  trailingSlash: false,
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
